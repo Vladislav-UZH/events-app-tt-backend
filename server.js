7b900862ebe293801123182998b3a4ae64e40ecc
Vladislav-UZH <perebziak.vladislav@gmail.com>
2023-04-24 19:54:50 +0300

    fixed

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const app = require("./app");
-require("dotenv").config();
-const db = require("./db/connection");
+const app = require('./app');
+require('dotenv').config();
+const db = require('./db/connection');
 
-const PORT = process.env.PORT || 9090;
+const PORT = process.env.PORT || 8080;
 const startServer = async () => {
   try {
     await db();
-    app.listen(PORT, (e) => {
+    app.listen(PORT, e => {
       if (e) {
-        console.error("Server launch failed", e);
+        console.error('Server launch failed', e);
         return;
       }
-      console.log("Database connected successfully");
+      console.log('Database connected successfully');
       console.log(`Server running. Use our API on port: ${PORT}`);
     });
   } catch (e) {
-    console.error("Database connection error", e);
+    console.error('Database connection error', e);
   }
 };
 
